dc7c47eca17d1da6aecff97b282483cf317f9c9e
SoftechPundits <softechpundits@gmail.com>
2019-11-12 20:02:55 -0500

    new file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Pattern-Practice-with-Python
- This is my source written while practicing Python
+# Pattern Practice with Python
+ 
+This repository is a compilation of my source code written while trying to pickup Python.
+ See LICENSE for more info on usage permissions.
+
---
+++ pat9.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 10:22:09 2019
+
+@author: nehap
+"""
+
+"""
+Input: 5
+Output :
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+"""
+if __name__=="__main__": 
+    n =  int(input("Input: "))
+    #Initial spaces
+    k = n-1
+    print("Output :")
+    #Outer Loop - controlling number of rows
+    for i in range(0, n, 1):
+        #Inner loop - controlling spaces
+        for j in range(0,k):
+            print(end=" ")
+        #Inner loop - controlling stars
+        for j in range(0,i+1):
+             print("*", end=" ")
+        #Increment spcae after each row
+        k=k-1
+        print("\r")
+       
+        
+        
+        
+      
